2a7b051410337b0485257f5b79f5d6fab7bb3194
Leeky[Suya] <kocetovaanastasia19@gmail.com>
2026-01-16 14:08:48 +0300

    йоу

---
+++ javascript/script.js
@@ -0,0 +1,20 @@
+let welcome = "Привеееет йоу йоу";
+
+alert(welcome);
+console.log(welcome);
+
+let a = +prompt("A = ");
+let b = +prompt("B = ");
+console.log(a*b);
+
+let age = +prompt("Age = ");
+if(age < 18){
+    alert("You cant go here");
+}
+else{
+    alert("WElcome");
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(5 * i + '');
+}
